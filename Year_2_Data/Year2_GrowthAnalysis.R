8f6e313fc895ad065b039f680c1a639714334f9a
skmontague23 <skmontague23@gmail.com>
2025-10-01 20:47:39 -0400

    working with year 2 growth data

---
+++ Year_2_Data/Year2_GrowthAnalysis.R
@@ -0,0 +1,205 @@
+#load packages
+library(lme4) #for linear mixed effects model
+library(readr) #to read in data
+library(tidyr)
+library(emmeans) #for post hocs
+library(esquisse) #interface for building plots
+library(dplyr) #for data wrangling
+library(nlme) #for linear mixed effects model
+library(car) #for the Levene test
+library(ggplot2) #for graphs
+library(plotrix) #for standard error
+library(performance) #model testing
+library(lmtest) #another option than levene test for variance
+
+R.version.string
+citation("lme4")
+
+#Read in dataset, set column types
+getwd()
+setwd("/Users/sophiemontague/Desktop/MontagueORCC_repo/MontagueORCC/Oyster_Weight_Data")
+
+Year2_Growth <- read_csv("/Users/sophiemontague/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data/growth_YEAR2_weights_merged.csv", 
+                             col_types = cols(Phase_1_temp = col_factor(), 
+                                              Phase_1_DO =col_factor(), 
+                                              Phase_2.1_temp =col_factor(), 
+                                              Phase_2.1_DO=col_factor(), 
+                                              Phase_1_rep =col_factor(), 
+                                              Phase_2_rep =col_factor(),
+                                              Phase_1_treat = col_factor(),
+                                              Phase_2_treat = col_factor(),
+                                              Phase_2_rep_R = col_factor(),
+                                              Phase_1_rep_R = col_factor()))
+colnames(Year2_Growth)
+#filter out dead and untagged
+Year2_Growth_clean <- Year2_Growth%>%
+  drop_na(Actual_tissue_growth_year2_mg) %>%
+  filter(Exclude_year2 != "Y" | is.na(Exclude_year2)) %>%
+  mutate(
+    Actual_tissue_growth_year2_mg = as.numeric(Actual_tissue_growth_year2_mg),
+    Actual_shell_growth_year2_mg = as.numeric(Actual_shell_growth_year2_mg),
+    Ratio_TS_year2 = as.numeric(Ratio_TS_year2))
+
+#check levels
+str(Year2_Growth_clean)
+levels(Year2_Growth_clean$Phase_1_DO)
+levels(Year2_Growth_clean$Phase_1_temp)
+levels(Year2_Growth_clean$Phase_2.1_DO)
+levels(Year2_Growth_clean$Phase_2.1_temp)
+levels(Year2_Growth_clean$Phase_1_treat)
+levels(Year2_Growth_clean$Phase_2_treat)
+
+##QAQC
+#look at minimum tissue growth
+sorted_vector <- sort(Year2_Growth_clean$Actual_tissue_growth_year2_mg)
+lowest_30_values <- sorted_vector[1:30]
+lowest_30_values
+
+#look at minimum shell growth
+min(Year2_Growth_clean$Actual_shell_growth_year2_mg)
+sorted_vector <- sort(Year2_Growth_clean$Actual_shell_growth_year2_mg)
+lowest_30_values <- sorted_vector[1:30]
+lowest_30_values
+
+
+
+#set contrasts ALWAYS RUN
+options(contrasts = c("contr.sum","contr.poly")) #could also be contr.treatment for unequal groups sum
+getOption("contrasts") 
+
+
+##tissue growth (mg), having the Actual_tissue_pre_mg as the covariate makes different post hocs significant
+m1 <- lmer(Actual_tissue_growth_year2_mg~ Phase_1_DO*Phase_1_temp*Phase_2.1_temp*Phase_2.1_DO+Actual_tissue_growth_mg+
+             (1|Phase_2_rep_R)+(1|Phase_1_rep_R)+
+             (1|Phase_2_rep_R:Phase_1_DO)+(1|Phase_2_rep_R:Phase_1_temp)+(1|Phase_2_rep_R:Phase_1_DO:Phase_1_temp), data = Year2_Growth_clean, REML=TRUE)
+Anova(m1, test="F", type="III")
+
+#posthocs
+emmeans(m1,specs = pairwise ~ Phase_1_temp*Phase_2.1_temp, adjust = "none")
+emmeans(m1,specs = pairwise ~ Phase_1_temp:Phase_2.1_temp:Phase_2.1_DO, adjust = "none")
+
+#diagnostics
+leveneTest(Actual_tissue_growth_year2_mg~Phase_1_treat*Phase_2_treat, Year2_Growth_clean) 
+m1.e <- residuals(m1) 
+qqnorm(m1.e)
+qqline(m1.e)
+
+#plot with mean and SD, 16 point TISSUE
+summary_stats_t <- Year2_Growth_clean %>%
+  group_by(Phase_1_treat, Phase_2_treat) %>%
+  mutate(
+    mean_growth = mean(Actual_tissue_growth_year2_mg, na.rm = TRUE),
+    se_growth = std.error(Actual_tissue_growth_year2_mg, na.rm = TRUE))
+
+#reorder Phase_1_treat and Phase_2_treat
+summary_stats_t$Phase_1_treat <- factor(summary_stats_t$Phase_1_treat, 
+                                        levels = c("Cont", "Warm","Hyp", "Both"))
+summary_stats_t$Phase_2_treat <- factor(summary_stats_t$Phase_2_treat, 
+                                        levels = c("Cont", "Warm","Hyp", "Both"))
+
+ggplot(summary_stats_t, aes(x = Phase_1_treat, y = mean_growth, color = Phase_1_treat)) +
+  geom_point(size = 4, position = position_dodge(0.9)) + # Plot means as points
+  geom_errorbar(aes(ymin = mean_growth - se_growth, ymax = mean_growth + se_growth), 
+                width = 0.2, position = position_dodge(0.9)) + # Error bars for SD
+  theme_classic(base_size = 20) +
+  guides(color = "none") + # Remove legend for color
+  facet_wrap(vars(Phase_2_treat), 
+             labeller = as_labeller(c("Hyp" = "Hypoxic", "Cont" = "Control", "Warm" = "Warm", "Both" = "Both")), 
+             scales = "fixed", nrow = 1) + # Facet by Phase_2_treat
+  scale_color_manual(values = c("Hyp" = "steelblue3", "Warm" = "palevioletred", "Cont" = "burlywood3", "Both" = "plum3")) +
+  labs(x = "Phase 1 Treatment", y = "Mean Normalized Tissue Growth (mg)") +
+  scale_x_discrete(labels = c("Hyp" = "Hypoxic", "Cont" = "Control", "Warm" = "Warm", "Both" = "Both")) +
+  theme(legend.position = "none") # Remove legend
+
+
+#plot with phase 1 and 2 temp, TISSUE
+summary_stats_t <- Year2_Growth_clean %>%
+  filter(!is.na(Actual_tissue_growth_year2_mg)) %>%
+  group_by(Phase_1_temp, Phase_2.1_temp) %>%
+  mutate(
+    mean_growth = mean(Actual_tissue_growth_year2_mg, na.rm = TRUE),
+    se_growth = std.error(Actual_tissue_growth_year2_mg, na.rm = TRUE))
+
+#reorder Phase_1_treat and Phase_2_treat
+summary_stats_t$Phase_1_temp <- factor(summary_stats_t$Phase_1_temp, 
+                                        levels = c("Ambient", "Warm"))
+summary_stats_t$Phase_2.1_temp <- factor(summary_stats_t$Phase_2.1_temp, 
+                                        levels = c("Ambient", "Warm"))
+
+ggplot(summary_stats_t, aes(x = Phase_1_temp, y = mean_growth, color = Phase_1_temp)) +
+  geom_point(size = 4, position = position_dodge(0.9)) + # Plot means as points
+  geom_errorbar(aes(ymin = mean_growth - se_growth, ymax = mean_growth + se_growth), 
+                width = 0.2, position = position_dodge(0.9)) + # Error bars for SD
+  theme_classic(base_size = 20) +
+  guides(color = "none") + # Remove legend for color
+  facet_wrap(vars(Phase_2.1_temp), 
+             labeller = as_labeller(c("Warm" = "Warm", "Ambient" = "Ambient")), 
+             scales = "fixed", nrow = 1) + # Facet by Phase_2_treat
+  labs(x = "Phase 1 Treatment", y = "Year 2 Tissue Growth (mg)") +
+  theme(legend.position = "none") # Remove legend
+
+
+
+
+## Shell growth
+##tissue growth (mg)
+pos_shell<- Year2_Growth_clean%>%
+  filter(Actual_shell_growth_year2_mg >= 0)
+
+m2 <- lmer(Actual_shell_growth_year2_mg~ Phase_1_DO*Phase_1_temp*Phase_2.1_temp*Phase_2.1_DO+Actual_tissue_post_mg+
+             (1|Phase_2_rep_R)+(1|Phase_1_rep_R)+
+             (1|Phase_2_rep_R:Phase_1_DO)+(1|Phase_2_rep_R:Phase_1_temp)+(1|Phase_2_rep_R:Phase_1_DO:Phase_1_temp), data = pos_shell, REML=TRUE)
+Anova(m2, test="F", type="III")
+
+#posthocs
+emmeans(m2,specs = pairwise ~ Phase_1_temp*Phase_2.1_temp, adjust = "none") 
+
+#diagnostics
+leveneTest(Actual_shell_growth_year2_mg~Phase_1_treat*Phase_2_treat, Year2_Growth_clean) 
+m1.e <- residuals(m2) 
+qqnorm(m1.e)
+qqline(m1.e)
+
+
+summary_stats_s <- Year2_Growth_clean %>%
+  group_by(Phase_1_treat, Phase_2_treat) %>%
+  mutate(
+    mean_growth = mean(Actual_shell_growth_year2_mg, na.rm = TRUE),
+    se_growth = std.error(Actual_shell_growth_year2_mg, na.rm = TRUE))
+
+#reorder Phase_1_treat and Phase_2_treat
+summary_stats_s$Phase_1_treat <- factor(summary_stats_s$Phase_1_treat, 
+                                        levels = c("Cont", "Warm","Hyp", "Both"))
+summary_stats_s$Phase_2_treat <- factor(summary_stats_s$Phase_2_treat, 
+                                        levels = c("Cont", "Warm","Hyp", "Both"))
+
+ggplot(summary_stats_s, aes(x = Phase_1_treat, y = mean_growth, color = Phase_1_treat)) +
+  geom_point(size = 4, position = position_dodge(0.9)) + # Plot means as points
+  geom_errorbar(aes(ymin = mean_growth - se_growth, ymax = mean_growth + se_growth), 
+                width = 0.2, position = position_dodge(0.9)) + # Error bars for SD
+  theme_classic(base_size = 20) +
+  guides(color = "none") + # Remove legend for color
+  facet_wrap(vars(Phase_2_treat), 
+             labeller = as_labeller(c("Hyp" = "Hypoxic", "Cont" = "Control", "Warm" = "Warm", "Both" = "Both")), 
+             scales = "fixed", nrow = 1) + # Facet by Phase_2_treat
+  scale_color_manual(values = c("Hyp" = "steelblue3", "Warm" = "palevioletred", "Cont" = "burlywood3", "Both" = "plum3")) +
+  labs(x = "Phase 1 Treatment", y = "Year 2 Tissue Growth (mg)") +
+  scale_x_discrete(labels = c("Hyp" = "Hypoxic", "Cont" = "Control", "Warm" = "Warm", "Both" = "Both")) +
+  theme(legend.position = "none") # Remove legend
+
+
+#####tissue:Shell growth (mg)
+colnames(Year2_Growth_clean)
+m3 <- lmer(Ratio_TS_year2~ Phase_1_DO*Phase_1_temp*Phase_2.1_temp*Phase_2.1_DO+
+             (1|Phase_2_rep_R)+(1|Phase_1_rep_R)+
+             (1|Phase_2_rep_R:Phase_1_DO)+(1|Phase_2_rep_R:Phase_1_temp)+(1|Phase_2_rep_R:Phase_1_DO:Phase_1_temp), data = Year2_Growth_clean, REML=TRUE)
+Anova(m3, test="F", type="III")
+
+#posthocs
+emmeans(m3,specs = pairwise ~ Phase_1_temp*Phase_2.1_temp, adjust = "none") 
+
+#diagnostics
+leveneTest(Ratio_TS_year2~Phase_1_treat*Phase_2_treat, Year2_Growth_clean) 
+m1.e <- residuals(m3) 
+qqnorm(m1.e)
+qqline(m1.e)
---
+++ Year_2_Data/Year2_datasetformatting.R
@@ -0,0 +1,237 @@
+#set working directory
+setwd("/Users/sophiemontague/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data")
+getwd()
+
+#load packages
+library(lme4) #for linear mixed effects model
+library(readr) #to read in data
+library(emmeans) #for post hocs
+library(esquisse) #interface for building plots
+library(dplyr) #for data wrangling
+library(nlme) #for linear mixed effects model
+library(car) #for the Levene test
+library(ggplot2) #for graphs
+library(plotrix) #for standard error
+library(performance) #model testing
+library(lmtest) #another option than levene test for variance
+
+#read in data
+df <- read_csv("Oysters to pull for Gonads.csv")
+df <- read_csv("Oysters to pull for Spawning.csv") #Also assigning phase 1 information to this sheet
+df <- read_csv("growth_YEAR2_weights_raw.csv")
+
+colnames(df)
+View(df)
+
+#adapted from JGM's code
+#adding phase 1 treatment based on tag color
+
+# white = control
+# orange = both
+# blue = hypoxic
+# green = warm
+
+for (i in 1:length(df$Tag_color)) { # loop through each row in df
+  tag <- df$Tag_color[i] # read that row's tag color
+  if (tag == 'B') { # if the tag color is blue, it's hypoxic
+    df$Phase_1_treat[i] <- 'Hyp'
+  } else if (tag == 'G') { # if the tag color is green, it's warm
+    df$Phase_1_treat[i] <- 'Warm'
+  } else if (tag == 'O') { # if the tag color is orange, it's both
+    df$Phase_1_treat[i] <- 'Both'
+  } else if (tag == 'W') { # if the tag color is white, it's control
+    df$Phase_1_treat[i] <- 'Cont'
+  } else if (is.na(tag)) {
+    df$Phase_1_treat[i] <- NA
+  }
+}
+
+
+##year 2 data, using dplyr to deal with missing values for tag color
+df$Phase_1_treat <- NA_character_  # start with all NAs
+df$Phase_1_treat[df$Tag_color == "B"] <- "Hyp"
+df$Phase_1_treat[df$Tag_color == "G"] <- "Warm"
+df$Phase_1_treat[df$Tag_color == "O"] <- "Both"
+df$Phase_1_treat[df$Tag_color == "W"] <- "Cont"
+
+df <- df %>%
+  mutate(Phase_1_treat = case_when(
+    Tag_color == "B" ~ "Hyp",
+    Tag_color == "G" ~ "Warm",
+    Tag_color == "O" ~ "Both",
+    Tag_color == "W" ~ "Cont",
+    TRUE ~ NA_character_   # keep NA if no match
+  ))
+
+# assigning tag numbers to phase 1 rep
+rep1 <- c(1,2,13,14,25,26,37,38,49,50,61,62)
+rep2 <- c(3,4,15,16,27,28,39,40,51,52,63,64)
+rep3 <- c(5,6,17,18,29,30,41,42,53,54,65,66)
+rep4 <- c(7,8,19,20,31,32,43,44,55,56,67,68)
+rep5 <- c(9,10,21,22,33,34,45,46,57,58,69,70)
+rep6 <- c(11,12,23,24,35,36,47,48,59,60,71,72)
+
+# changing to numeric class
+df$Tag_num <- as.numeric(df$Tag_num)
+
+# initialize empty list to store unmatched tag numbers
+unmatched_rows <- list()# assigning tag numbers to phase 1 rep
+rep1 <- c(1,2,13,14,25,26,37,38,49,50,61,62)
+rep2 <- c(3,4,15,16,27,28,39,40,51,52,63,64)
+rep3 <- c(5,6,17,18,29,30,41,42,53,54,65,66)
+rep4 <- c(7,8,19,20,31,32,43,44,55,56,67,68)
+rep5 <- c(9,10,21,22,33,34,45,46,57,58,69,70)
+rep6 <- c(11,12,23,24,35,36,47,48,59,60,71,72)
+
+# initialize empty list to store unmatched tag numbers
+unmatched_rows <- list()
+
+# convert list to df
+unmatched_df <- do.call(rbind, unmatched_rows)
+# print data frame of odd tag numbers
+unmatched_df
+
+# for loop to add phase 1 rep info
+for (i in 1:length(df$Tag_num)) { # loop through each row in df
+  tag <- df$Tag_num[i] # read the tag number 
+  if (tag %in% rep1) { # if tag number is in the rep1 list, the rep is 01
+    df$Phase_1_rep[i] <- '01'
+  } else if (tag %in% rep2) {
+    df$Phase_1_rep[i] <- '02'
+  } else if (tag %in% rep3) {
+    df$Phase_1_rep[i] <- '03'
+  } else if (tag %in% rep4) {
+    df$Phase_1_rep[i] <- '04'
+  } else if (tag %in% rep5) {
+    df$Phase_1_rep[i] <- '05'
+  } else if (tag %in% rep6) {
+    df$Phase_1_rep[i] <- '06'
+  } else { # if there are any tag numbers that don't match 
+    unmatched_rows[[length(unmatched_rows) + 1]] <- df[i, ] # add to list
+  }
+}
+
+View(df)
+# then write csv file
+write.csv(df, '/Users/sophiemontague/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data/growth_YEAR2_weights_working.csv')
+
+
+#check if the phase 1 treatment and reps are even
+sum(df$Phase_1_treat == "Cont")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "01")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "02")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "03")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "04")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "05")
+sum(df$Phase_1_treat == "Cont" & df$Phase_1_rep == "06")
+sum(df$Phase_1_treat == "Warm")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "01")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "02")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "03")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "04")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "05")
+sum(df$Phase_1_treat == "Warm" & df$Phase_1_rep == "06")
+sum(df$Phase_1_treat == "Hyp")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "01")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "02")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "03")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "04")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "05")
+sum(df$Phase_1_treat == "Hyp" & df$Phase_1_rep == "06")
+sum(df$Phase_1_treat == "Both")
+
+sum(df$Phase_1_rep == "01")
+as.data.frame(df$Phase_1_rep == "01")
+sum(df$Phase_1_rep == "02")
+sum(df$Phase_1_rep == "03")
+sum(df$Phase_1_rep == "04")
+sum(df$Phase_1_rep == "05")
+sum(df$Phase_1_rep == "06")
+
+#one that got put in rep 1 was supposed to go in rep 5. for gonads
+
+sum(df$Phase_2_treat == "Cont")
+sum(df$Phase_2_treat == "Warm")
+sum(df$Phase_2_treat == "Hyp")
+sum(df$Phase_2_treat == "Both")
+
+
+
+##create sample names from this information in excel
+
+
+##make sure that these oysters were not pulled for genetics last year (2024)
+library(readr)
+phase2_genetics <- read_csv("~/Desktop/CE_ORCC/2024/genetics/growth_weights_phase2.1genetics.csv")
+phase2_spawning <- read_csv("~/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data/Oysters_to pull_Spawning.csv")
+phase2_gonads <- read_csv("~/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data/Oysters_topull_gonads.csv")
+
+View(phase2_spawning)
+View(phase2_gonads)
+
+#Compare genetics and spawning
+#Get the vectors of Sample_Name from each dataframe
+phase2_genetics_names <- phase2_genetics$Sample_Name
+phase2_spawning_names <- phase2_spawning$Sample_name
+phase2_gonad_names <- phase2_gonads$Sample_name
+
+View(phase2_gonad_names)
+
+#Find the intersection and count overlap
+overlap_names <- intersect(phase2_genetics_names, phase2_spawning_names)
+length(overlap_names)
+View(overlap_names)
+
+
+#now genetics and gonads
+#Find the intersection and count overlap
+overlap_names <- intersect(phase2_genetics_names, phase2_gonad_names)
+length(overlap_names) #62 oysters lost shell area AND length out of 92
+View(overlap_names)
+
+
+#now spawning and gonads
+#Find the intersection and count overlap
+overlap_names <- intersect(phase2_spawning_names, phase2_gonad_names)
+length(overlap_names) #62 oysters lost shell area AND length out of 92
+View(overlap_names)
+
+
+
+
+#### Year 2 ####
+?right_join()
+
+x <- read_csv("~/Desktop/MontagueORCC_repo/MontagueORCC/Oyster_Weight_Data/growth_phase2.1_areaMERGED.csv")
+y <- read_csv("growth_YEAR2_weights_working.csv")
+View(x);View(y)
+
+colnames(x)
+
+Year2_merged <- right_join(
+  x,
+  y,
+  by = c("Sample_Name", "Phase_1_treat", "Phase_2_treat"),
+  keep = NULL,
+  na_matches = "never",
+  unmatched = "drop"
+)
+
+View(Year2_merged)
+nrow(y)
+nrow(Year2_merged)
+
+## see which rows didnt match, these should all have NAs from tags falling off
+unmatched_y <- anti_join(
+  y,
+  x,
+  c("Sample_Name", "Phase_1_treat", "Phase_2_treat")
+)
+
+View(unmatched_y) # yes they all have an NA from missing tag info
+
+#write CSV file with newly joined data
+write.csv(Year2_merged, '/Users/sophiemontague/Desktop/MontagueORCC_repo/MontagueORCC/Year_2_Data/growth_YEAR2_weights_merged.csv')
+
+
+
